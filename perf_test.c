ca3d69aed391dfaebc360e908ca8ae203fa7da24
Yu-Lang Chu <yulang.chu@gmail.com>
2021-04-02 17:50:19 +0200

    Update test sampling

--- perf_test.c
+++ perf_test.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include "threadpool.h"
 
+
 #define PRECISION 100 /* upper bound in BPP sum */
 #define NANOSEC 1e9
 
@@ -40,16 +41,40 @@ static double run(tpool_t pool){
 int main()
 {
     const int size = 1000;
+    const int size_sampling = 100;
     struct timespec t_start, t_end;
     long long elapsed_time[size];
     double bpp[size];
     tpool_t pool = tpool_create(4);
+
     for(int i = 0; i < size;i++){
-        clock_gettime(CLOCK_MONOTONIC, &t_start);
-        bpp[i] = run(pool);
-        clock_gettime(CLOCK_MONOTONIC, &t_end);
-        elapsed_time[i] = (t_end.tv_sec * NANOSEC + t_end.tv_nsec) -
-                          (t_start.tv_sec * NANOSEC + t_start.tv_nsec);
+        elapsed_time[i] = 0;
+        long long elapsed_time_tmp[size_sampling];
+        for(int j = 0; j < size_sampling;j++){
+            clock_gettime(CLOCK_MONOTONIC, &t_start);
+            bpp[i] = run(pool);
+            clock_gettime(CLOCK_MONOTONIC, &t_end);
+            elapsed_time_tmp[j] = (t_end.tv_sec * NANOSEC + t_end.tv_nsec) -
+                              (t_start.tv_sec * NANOSEC + t_start.tv_nsec);
+        }
+        long long sum = 0;
+        for(int k = 0; k < size_sampling;k++){
+            sum += elapsed_time_tmp[k];
+        }
+        long long mean = sum / size_sampling, sd = 0.0;
+        for(int k = 0; k < size_sampling;k++){
+           sd += (elapsed_time_tmp[k] - mean) * (elapsed_time_tmp[k] - mean);
+        }
+        sd = sqrt(sd/(size_sampling - 1));
+        int count = 0;
+        for(int k=0; k < size_sampling; k++){
+            if(elapsed_time_tmp[k] < (mean - 2*sd) && elapsed_time_tmp[k] > (mean + 2*sd)){
+                continue;
+            }
+            elapsed_time[i] +=  elapsed_time_tmp[k];
+            count++;
+        }
+        elapsed_time[i] = (elapsed_time[i]/count);
     }
     tpool_join(pool);
 
